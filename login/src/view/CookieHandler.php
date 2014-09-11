d7796e5dbb4cd15b155210aa6d45e5f420d3bca7
al223ec <al223ec@student.lnu.se>
2014-09-11 21:23:33 +0200

    Cookies

--- login/src/view/CookieHandler.php
+++ login/src/view/CookieHandler.php
@@ -3,27 +3,31 @@
 namespace view; 
 
 class CookieHandler{
-
+	//Behöver spara informationen till db
+	//Bör inte använda hashen i cookien
 	public function __construct(){
-		$this->saveHashedCookie("Password", "2a'$'10'$'DixWNrAgbkC7Z7T6JY7ex.g7hiqrXMF3qh9mBvI9CNQeWbeM.y5Tq"); 
-		var_dump($this->loadHashedCookie("Password", "2a'$'10'$'DixWNrAgbkC7Z7T6JY7ex.g7hiqrXMF3qh9mBvI9CNQeWbeM.y5Tq")); 
-	}
-	public function saveHashedCookie($cookieName, $cookieValue){
-		$hash = crypt($cookieValue, $this->generateRandomString());
-		$this->saveCookie($cookieName, $hash); 
+		//$this->saveHashedCookie("Password", "2a'$'10'$'DixWNrAgbkC7Z7T6JY7ex.g7hiqrXMF3qh9mBvI9CNQeWbeM.y5Tq"); 
+		//var_dump($this->loadAndConfirmHashedCookie("Password", "2a'$'10'$'DixWNrAgbkC7Z7T6JY7ex.g7hiqrXMF3qh9mBvI9CNQeWbeM.y5Tq")); 
 	}
 
-	public function saveCookie($cookieName, $cookieValue){
+	public function saveHashedCookie($cookieName){
+		$cookieValue = $this->generateRandomString(); 
+//		$hash = crypt($this->generateRandomString(), $this->generateRandomString());
+		$this->saveCookie($cookieName, $cookieValue); 
+	}
+
+	public function saveCookie($cookieName){
+		$cookieValue = $this->generateRandomString(); 
 		setcookie($cookieName, $cookieValue, -1); 
 	} 
 
 	public function loadCookie($cookieName){
+		throw new \Exception("CookieHandler::loadCookie Not implemented exception Processing Request");
 	}
 
-	public function loadHashedCookie($cookieName, $password){
-		$hashedPassword = isset($_COOKIE[$cookieName]) ? $_COOKIE[$cookieName] : ""; 
-		return crypt($password, $hashedPassword) === $hashedPassword; 
-		
+	public function loadAndConfirmHashedCookie($cookieName, $value){
+		$hashedValue = isset($_COOKIE[$cookieName]) ? $_COOKIE[$cookieName] : ""; 
+		return crypt($value, $hashedValue) === $hashedValue; 
 	}
 
 	private function generateRandomString($length = 16) {
